3d22e0b6b65935199cd2017af181a4cfbf5975fd
HaiHoang12071995 <52276215+HaiHoang12071995@users.noreply.github.com>
2019-08-23 16:07:18 +0700

    add--3

--- DataStructure/src/array/RotationArrayUseResever.java
+++ DataStructure/src/array/RotationArrayUseResever.java
@@ -10,7 +10,15 @@ public class RotationArrayUseResever {
 			end--;
 		}
 	}
-	
+	public void resever(int[] arr, int start, int end) {
+		while(start < end) {
+			int temp  = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
 	
 	
 	
